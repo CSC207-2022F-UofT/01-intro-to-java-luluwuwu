c4b010a3ca7a53e5601828957e2ac5866cb4c8c6
luluwu <luluwu.wu@mail.utoronto.ca>
2023-05-18 15:56:40 -0400

    complete task

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -7,6 +7,8 @@
  * relevant readings for each task.
  */
 
+import java.sql.SQLOutput;
+
 /**
  *  1. In Java, no code can exist outside of a class. Unlike Python which uses
  *    functions, all code in Java uses methods.
@@ -43,7 +45,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
-
+        System.out.println("Hello World!");
 
 
         /* 4. In Python, we could declare variables using a simple assignment
@@ -62,7 +64,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
-
+        int my_variable = 100;
 
 
         /* Do not remove the line below: if you did task 2 correctly, then
@@ -99,7 +101,10 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
+        for (int i = 10; i >= 0 ; i--){
+            System.out.println("Current count: " + i);
 
+        }
 
     }
 
@@ -141,8 +146,12 @@ public class Basics {
          *                you may use them if you wish.
          */
         StringBuilder ret = new StringBuilder();
+        for (int i = 0; i <= 6; i++){
+            ret = ret.append(to_split.split(" ")[i].charAt(0));
+        }
 
-        // Fill in the rest of the body here
+
+
 
         return ret.toString();
     }
@@ -170,6 +179,14 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
+        for (int i = 0; i < arr.length; i++){
+            if (arr.length == 1){
+                return current_sum;
+            } else if (i % 2 == 1) {
+                current_sum += arr[i];
+            }
+
+        }
 
         return current_sum;
     }
